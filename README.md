bea1b62eb09a30de1e2cd4bfd336626b9827e0a9
Krysmatic <christopherchangx1@gmail.com>
2021-02-23 22:22:05 -0800

    Update

---
+++ README.md
@@ -0,0 +1,39 @@
+![Banner](https://cdn.discordapp.com/attachments/596521261874675723/811320565352497152/AssetSearchBanner.jpg)
+[![License](https://img.shields.io/badge/License-MIT-brightgreen.svg)](LICENSE)
+
+# AssetID Search
+
+A Roblox Studio plugin that parses game files in search for assets given their IDs.
+
+# Install
+1. Navigate to https://www.roblox.com/library/6061120431/AssetID-Search
+2. Press Install
+
+# Features
+- Toggle-able settings for which directories to parse
+  - Workspace
+  - ReplicatedFirst
+  - ReplicatedStorage
+  - ServerStorage
+  - StarterGui
+  - Lighting
+  - SoundService
+  - StarterPack
+- Shows exact asset location paths
+- Able to locate multiple instances of assets
+- Prints results to output
+
+# Plugin User Interface
+
+A user-friendly UI streamlines the development process.
+
+<img src="https://doy2mn9upadnk.cloudfront.net/uploads/default/original/4X/d/7/a/d7aafc6f7a87583eb7f1a858a3241f5026fe8a98.png" width="30%" height="30%">
+
+# Example Use Case
+This is an example of how we can use this plugin to optimize performance. We can see a list of high memory textures in the developer console. With the IDs, we can use the plugin to locate them in Roblox Studio.
+
+<img src="https://doy2mn9upadnk.cloudfront.net/uploads/default/original/4X/9/5/0/95007504711dcf17eb0556a8dd8500e3e35dc358.jpeg" width="50%" height="50%">
+
+The plugin then prints asset paths to output.
+
+<img src="https://doy2mn9upadnk.cloudfront.net/uploads/default/original/4X/4/5/2/4521f55846cb0d6f2b667df0f95d4767168735da.png" width="50%" height="50%">
---
+++ src/Core/SearchModule.lua
@@ -0,0 +1,78 @@
+-- SearchModule.lua
+-- Krysmatic
+
+local SearchModule = {}
+
+function SearchModule:SearchGame(directory, SEARCH_ID)
+	local children = directory:GetChildren()
+	local output = {}
+
+	local function PrintResult(typeName, model)
+		print("------------------------------------------------------------------------------------")
+		print("[ASSET ID: " .. SEARCH_ID .. "] Found " .. typeName .. " in " .. directory.Name)
+
+		-- Get all parent names
+		local function GetParent(modelobj)
+			if modelobj.Parent then
+				GetParent(modelobj.Parent)
+				table.insert(output, " >> " .. modelobj.Name)
+			end
+		end
+
+		GetParent(model)
+		print(table.concat(output))
+	end
+	
+	local t = 0
+	for i = 1, #children do
+		t = t + 1
+		if t % 200 == 0 then
+			wait()
+		end
+		if children[i]:IsA("MeshPart") then
+			if string.match(tostring(children[i].TextureID), SEARCH_ID) then
+				PrintResult("Meshpart", children[i])
+			end
+		end
+		if children[i]:IsA("Decal") or children[i]:IsA("Texture") then
+			if string.match(tostring(children[i].Texture), SEARCH_ID) then
+				PrintResult("Decal/Texture", children[i])
+			end
+		end
+		if children[i]:IsA("ImageLabel") then
+			if string.match(tostring(children[i].Image), SEARCH_ID) then
+				PrintResult("ImageLabel", children[i])
+			end
+		end
+		if children[i]:IsA("ImageButton") then
+			if string.match(tostring(children[i].Image), SEARCH_ID) then
+				PrintResult("ImageButton", children[i])
+			end
+			if string.match(tostring(children[i].HoverImage), SEARCH_ID) then
+				PrintResult("ImageButton Hover", children[i])
+			end
+		end
+		if children[i]:IsA("Sound") then
+			if string.match(tostring(children[i].SoundId), SEARCH_ID) then
+				PrintResult("Sound", children[i])
+			end
+		end
+		self:SearchGame(children[i], SEARCH_ID)
+	end
+end
+
+-- Start search
+function SearchModule:Search(settings, SEARCH_ID)
+	if SEARCH_ID and SEARCH_ID ~= "" then
+		for service, props in pairs(settings) do
+			if props["Activated"] then
+				self:SearchGame(game[service], SEARCH_ID)
+			end
+		end
+		print("Finished Searching ID: " .. SEARCH_ID)
+	else
+		print("Enter valid ID.")
+	end
+end
+
+return SearchModule
